8e8b8c90518b21d8634922843deebf4b9dcd2eb4
Johana Supíková <johana.supikova@seznam.cz>
2021-11-08 12:33:23 +0100

    feat(admin): add option to generate login for service account
    * in some namespaces login should be generated rather than filled in for service accounts
    * this information is now extracted from namespaces rules same as in sponsored user
    * changed getting namespaces options from gui config to backend config, same as sponsored users

--- libs/perun/openapi/src/lib/api/membersManager.service.ts
+++ libs/perun/openapi/src/lib/api/membersManager.service.ts
@@ -1129,6 +1129,54 @@ export class MembersManagerService {
     }
 
     /**
+     * Get all members from all vos.
+     * @param observe set whether or not to return the data Observable as the body, response or events. defaults to returning the body.
+     * @param reportProgress flag to report request and response progress.
+     */
+    public getAllMembers(observe?: 'body', reportProgress?: boolean): Observable<Array<Member>>;
+    public getAllMembers(observe?: 'response', reportProgress?: boolean): Observable<HttpResponse<Array<Member>>>;
+    public getAllMembers(observe?: 'events', reportProgress?: boolean): Observable<HttpEvent<Array<Member>>>;
+    public getAllMembers(observe: any = 'body', reportProgress: boolean = false ): Observable<any> {
+
+        let headers = this.defaultHeaders;
+
+        // authentication (ApiKeyAuth) required
+        if (this.configuration.apiKeys && this.configuration.apiKeys["Authorization"]) {
+            headers = headers.set('Authorization', this.configuration.apiKeys["Authorization"]);
+        }
+
+        // authentication (BasicAuth) required
+        if (this.configuration.username || this.configuration.password) {
+            headers = headers.set('Authorization', 'Basic ' + btoa(this.configuration.username + ':' + this.configuration.password));
+        }
+        // authentication (BearerAuth) required
+        if (this.configuration.accessToken) {
+            const accessToken = typeof this.configuration.accessToken === 'function'
+                ? this.configuration.accessToken()
+                : this.configuration.accessToken;
+            headers = headers.set('Authorization', 'Bearer ' + accessToken);
+        }
+        // to determine the Accept header
+        const httpHeaderAccepts: string[] = [
+            'application/json'
+        ];
+        const httpHeaderAcceptSelected: string | undefined = this.configuration.selectHeaderAccept(httpHeaderAccepts);
+        if (httpHeaderAcceptSelected !== undefined) {
+            headers = headers.set('Accept', httpHeaderAcceptSelected);
+        }
+
+
+        return this.httpClient.get<Array<Member>>(`${this.configuration.basePath}/json/membersManager/getAllMembers`,
+            {
+                withCredentials: this.configuration.withCredentials,
+                headers: headers,
+                observe: observe,
+                reportProgress: reportProgress
+            }
+        );
+    }
+
+    /**
      * Return all loaded namespaces rules.
      * @param observe set whether or not to return the data Observable as the body, response or events. defaults to returning the body.
      * @param reportProgress flag to report request and response progress.
